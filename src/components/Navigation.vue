812d387816d20990334c50fdb6b3487d4dc69f94
Ingo Dahn <ingodahn@gmain.com>
2018-11-03 17:46:56 +0100

    Added navigation

---
+++ src/components/Navigation.vue
@@ -0,0 +1,53 @@
+<template>
+    <div>
+        <ul>
+            <li><a href="#context">Kontext</a></li>
+            <li><a href="#system">Systemauswahl</a></li>
+            <li><a href="#upload">Daten hochladen</a></li>
+            <li><a href="#testType">Art des Tests</a></li>
+            <div v-if="QuestionsNr > 0">
+                <li><a href="#scoreDistribution">Punkteverteilung</a></li>
+                <li><a href="#less">Wenig Punkte</a></li>
+                <li><a href="#more">Viele Punkte</a></li>
+                <li><a href="#attempts">Versuche</a></li>
+                <li><a href="#best">Die Besten</a></li>
+            </div>
+            <div v-else>
+                <li class="inactive">Punkteverteilung</li>
+                <li class="inactive">Wenig Punkte</li>
+                <li class="inactive">Versuche</li>
+                <li class="inactive">Die Besten</li>
+            </div>
+        </ul>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "Navigation",
+    props: ["QuestionsNr"]
+}
+//eslint-disable-next-line
+//console.log(this.QuestionsNr);
+</script>
+
+<style scoped>
+    li {
+         list-style: none;
+    }
+    a {
+        display: block;
+        padding: 4px 0 4px 10px;
+        border-width: 0 0 1px 0;
+        border-style: dotted;
+        text-decoration: none;
+    }
+    .inactive {
+        display: block;
+        padding: 4px 0 4px 10px;
+        border-width: 0 0 1px 0;
+        border-style: dotted;
+        color: grey;
+    }
+</style>
+
--- src/components/Test.vue
+++ src/components/Test.vue
@@ -1,51 +1,64 @@
 <template>
   <div id="Test" >
-    <div id="container">
-    <div id="head">
-      <h1>Test-Analyse</h1>
-       <p>Diese Webseite kann die Ergebnisse Ihrer Tests analysieren. Sie kann Ihnen dabei helfen, die Qualität Ihrer Testfragen einzuschätzen. </p>
-       <p>Die Qualität einer Testfrage ist auch abhängig von der Zielstellung des jeweiligen Tests. So wird man bei einem Test zur Motivierung der Studierenden oder bei einem Test zum Üben eine höhere Erfolgsquote erwarten, als bei einem Diagnose-Test, der die Grenzen von Wissen, Können und Kompetenzen der Studierenden ausloten soll.</p>
-       <p> <b>Datenschutzhinweis:</b> Diese Webseite überträgt nach ihrem Aufruf keine Daten an andere Systeme. Alle Berechnungen erfolgen ausschließlich in Ihrem Browser.</p>
-       <p> <b>Bitte bachten Sie:</b> Diese Software hat experimentellen Charakter. Es wird keinerlei Garantie übernommen. Hinweise auf Probleme und Wünsche zur Verbesserung der Seite sind jedoch <a href="mailto:dahn@dahn-research.eu">ausdrücklich erwünscht</a>.</p>
-      
+    <div class="container">
+      <div class="page-title">
+        <h1>Test-Analyse</h1>
+      </div>
+      <div class="navigation">
+        <Navigation :QuestionsNr="questionsNr"></Navigation>
+      </div>
+      <div class="main">
+        <div id="context">
+          <p>Diese Webseite kann die Ergebnisse Ihrer Tests analysieren. Sie kann Ihnen dabei helfen, die Qualität Ihrer Testfragen einzuschätzen. </p>
+          <p>Die Qualität einer Testfrage ist auch abhängig von der Zielstellung des jeweiligen Tests. So wird man bei einem Test zur Motivierung der Studierenden oder bei einem Test zum Üben eine höhere Erfolgsquote erwarten, als bei einem Diagnose-Test, der die Grenzen von Wissen, Können und Kompetenzen der Studierenden ausloten soll.</p>
+          <p> <b>Datenschutzhinweis:</b> Diese Webseite überträgt nach ihrem Aufruf keine Daten an andere Systeme. Alle Berechnungen erfolgen ausschließlich in Ihrem Browser.</p>
+          <p> <b>Bitte bachten Sie:</b> Diese Software hat experimentellen Charakter. Es wird keinerlei Garantie übernommen. Hinweise auf Probleme und Wünsche zur Verbesserung der Seite sind jedoch <a href="mailto:dahn@dahn-research.eu">ausdrücklich erwünscht</a>.</p>
+        </div>
+    <div id="system">
       <h2>Bitte wählen Sie Ihr System</h2>
         <input type="radio" id="imathas" v-model="system" value="IMathAS" v-on:click="reset">
         <label for="IMathAS">IMathAS</label>
         <input type="radio" id="ilias" v-model="system" value="Ilias" v-on:click="reset"> 
         <label for="Ilias">Ilias</label>
-        
     </div>
-    <h2>Laden Sie Ihre Testdaten hoch</h2>
+    <div id="upload">
+      <h2>Laden Sie Ihre Testdaten hoch</h2>
 
-    <div id="intro">
-      <TestReaderIMathAS v-if="system == 'IMathAS'" v-on:testRead="testread"></TestReaderIMathAS>
-      <TestReaderIlias v-if="system == 'Ilias'" v-on:testRead="testread"></TestReaderIlias>
-      
+      <div id="intro">
+        <TestReaderIMathAS v-if="system == 'IMathAS'" v-on:testRead="testread"></TestReaderIMathAS>
+        <TestReaderIlias v-if="system == 'Ilias'" v-on:testRead="testread"></TestReaderIlias>
+        
+      </div>
     </div>
     
-    <div v-if='questionsNr != 0'>
+    <div id="basics" v-if='questionsNr != 0'>
       <p>Der Test hat {{questionsNr}} Fragen. Es liegen Daten von {{studentsNr}} Studierenden vor. Maximal können {{ totalScore }} Pukte erreicht werden.</p>
       <p v-if='2*questionsNr >= studentsNr'><b>Für aussagekräftige Ergebnisse sollte es wenigstens doppelt so viele Studierende wie Fragen geben.</b></p>
       <EditMaxScores v-if="system == 'Ilias'" :Questions=questions></EditMaxScores>
     </div>
-    <SetType :testtype=type v-on:typeselected ="settype"></SetType>
-
-    <ScoreDistribution :ScoredSorted=scoredSorted :TotalScore=totalScore :Charts="['scoreDistribution']" :Questions = questions></ScoreDistribution>
-    <Less :Score=score></Less>
-    <More :Score=score></More>
-    <Attempts :Questions=questions></Attempts>
     
-    <BestStudents :Students=students :ScoredSorted=scoredSorted :Questions=questions></BestStudents>
+    <SetType id="testType" :testtype=type v-on:typeselected ="settype"></SetType>
 
-    <div class="push"></div>
+    <ScoreDistribution id="scoreDistribution" :ScoredSorted=scoredSorted :TotalScore=totalScore :Charts="['scoreDistribution']" :Questions = questions></ScoreDistribution>
+    <Less id="less" :Score=score></Less>
+    <More id="more" :Score=score></More>
+    <Attempts id="attempts" :Questions=questions></Attempts>
+    
+    <BestStudents id="best" :Students=students :ScoredSorted=scoredSorted :Questions=questions></BestStudents>
     </div>
+    <!--
+    <div class="push"></div>
+    -->
     <div class="footer">
       <p>&copy;Ingo Dahn (Dahn-Research), Lizenz: <a href="https://creativecommons.org/licenses/by-sa/3.0/de/">CC-BY-SA 3.0</a></p>
     </div>
+    </div>
+    
   </div>
 </template>
 
 <script>
+import Navigation from "./Navigation.vue";
 import SetType from "./SetType.vue";
 import TestReaderIMathAS from "./IMathAS/TestReader.vue";
 import TestReaderIlias from "./Ilias/TestReader.vue";
@@ -56,13 +69,11 @@ import BestStudents from "./BestStudents.vue";
 import EditMaxScores from "./EditMaxScores.vue";
 import ScoreDistribution from "./ScoreDistribution.vue";
 
-
-
 export default {
   name: "Test",
   data() {
     return {
-      system: 'IMathAS',
+      system: "IMathAS",
       type: "compulsory",
       questionsNr: 0,
       studentsNr: 0,
@@ -71,6 +82,7 @@ export default {
     };
   },
   components: {
+    Navigation,
     SetType,
     TestReaderIMathAS,
     TestReaderIlias,
@@ -86,10 +98,10 @@ export default {
       this.type = typeval;
     },
     reset: function() {
-      this.questionsNr=0;
-      this.studentsNr=0;
-      this.questions=[];
-      this.studentNames=[];
+      this.questionsNr = 0;
+      this.studentsNr = 0;
+      this.questions = [];
+      this.studentNames = [];
     },
     testread: function(test) {
       this.questionsNr = test.questionsNr;
@@ -123,8 +135,8 @@ export default {
       return scores;
     },
     totalScore: function() {
-      var tScore=0;
-      for (var i=0;i < this.questionsNr; i++) {
+      var tScore = 0;
+      for (var i = 0; i < this.questionsNr; i++) {
         // maxScore can be string if modified by input
         tScore += Number(this.questions[i].maxScore);
       }
@@ -177,22 +189,49 @@ function studentScore(s) {
 </script>
 
 <style scoped>
+.page-title {
+  grid-column: 1/3;
+  left: 0;
+  top: 0;
+  bottom: 0;
+  width: 100%;
+  background-color: hsl(198, 65%, 40%);
+  color: white;
+  text-align: center;
+}
+.navigation {
+  top: 75px;
+  left: 0;
+  height: 100vh;
+  border-width: 1px;
+  border-right-style: dashed;
+  padding: 1em;
+  grid-column: 1/2;
+  grid-row: 2;
+  position: sticky;
+}
 .footer {
-   position: fixed;
-   left: 0;
-   bottom: 0;
-   width: 100%;
-   height: 50px;
-   background-color: hsl(198, 65%, 40%);
-   color: white;
-   text-align: center;
+  height: 50px;
+  grid-column: 1/3;
+  grid-row: 3;
+  
+  background-color: hsl(198, 65%, 40%);
+  color: white;
+  text-align: center;
 }
 .push {
   height: 50px;
 }
 .container {
+  display: grid;
+  grid-gap: 10px;
+  grid-template-columns: auto auto;
+  grid-template-rows: auto 1fr auto;
   min-height: 100%;
-  margin-bottom: -50px;
+  padding: 20px;
+}
+.main {
+  padding: 20px;
 }
 </style>
 
