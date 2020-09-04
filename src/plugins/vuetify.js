21870c86fcdf9012d830a5c9fec1c616c32c411c
Ingo Dahn <ingodahn@gmail.com>
2020-09-04 11:21:00 +0200

    Vuetified navigation

---
+++ src/plugins/vuetify.js
@@ -0,0 +1,15 @@
+import Vue from 'vue';
+import Vuetify from 'vuetify/lib';
+
+Vue.use(Vuetify);
+
+export default new Vuetify({
+  theme: {
+    themes: {
+      light: {
+        primary: '#2481a8',
+        secondary: 'yellow'
+      }
+    }
+  }
+});
